371b66413260e5c7899d94389ea5fe2ad96cf325
josephdo11302 <129983235+josephdo11302@users.noreply.github.com>
2023-08-13 16:30:56 -0400

    Added code to pdfscrape.py to extract data and print to CSVs, also added unit tests to test these functions

--- main.py
+++
@@ -1,7 +0,0 @@
-from Model.pdfscrape import PDFScrape
-
-file_path = '/Users/dom/Final_project/test_result1.pdf'  # Path to a sample PDF file for testing
-scraper = PDFScrape(file_path)
-
-data_frames = scraper.extract_data()
-scraper.csv_output(data_frames)
